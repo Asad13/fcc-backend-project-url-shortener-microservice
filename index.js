112a0589b444d385516e2b65bfb3b1ba76e8ea18
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2022-07-14 14:41:16 +0600

    C12

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ app.post('/api/shorturl',(req,res) => {
   });
 });
 
-app.get('/api/shorturl/:short_url',(req,res) => {
-  const url = Url.findOne({short_url: parseInt(req.params.short_url)});
+app.get('/api/shorturl/:short_url',async (req,res) => {
+  const url = await Url.findOne({short_url: parseInt(req.params.short_url)});
   res.send(url);
 });
 
